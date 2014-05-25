74132934be764bc08e46bec7cce06a6286678444
ThomasAuberson <thomas.auberson@gmail.com>
2014-05-25 14:25:20 +1200

    Basic Sounds Basic COlliders for first 4 prefab tiles

--- assets/Scripts/PlayerBoat/PlayerMovement.cs
+++ assets/Scripts/PlayerBoat/PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 /*
@@ -16,73 +16,75 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
 	
-		//public float turnAcceleration = 0.1; // Rate at which turning accelerates
-		public float turnRate = 1.0f; // Rate at which character turns
-		public float defaultMoveSpeed = 1.0f; // Default speed at which character moves
-		public float defaultAcceleration = 0.001f; // Default rate at which the character gradually accelerates throughout game
+	//public float turnAcceleration = 0.1; // Rate at which turning accelerates
+	public float turnRate = 1.0f; // Rate at which character turns
+	public float defaultMoveSpeed = 1.0f; // Default speed at which character moves
+	public float defaultAcceleration = 0.001f; // Default rate at which the character gradually accelerates throughout game
 	
-		public float currentThrust = 0.01f;
-
-		// When player is dead
-		private bool sinking = false;
-
-		private GameObject gameController;
-
-
-		// Use this for initialization
-		void Start ()
-		{
-				gameController = GameObject.FindGameObjectWithTag (Tags.gameController);
-		}
+	public float currentThrust = 0.01f;
+		
+	// When player is dead
+	private bool sinking = false;
 	
-		// Update is called once per frame
-		void FixedUpdate ()
-		{
-				if (sinking) {
-						return;
-				}
-
-				float hInput = Input.GetAxis ("Horizontal"); 
-				//float vInput = Input.GetAxis ("Vertical");
+	private GameObject gameController;
+	
+	
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		gameController = GameObject.FindGameObjectWithTag (Tags.gameController);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (sinking) {
+			return;
+		}
 		
-				// Roatation
-				float turnAmount = hInput * turnRate;
-				rigidbody.AddForceAtPosition (-transform.right * turnAmount, transform.position + transform.forward * -3);
-				//transform.Rotate (0, turnAmount, 0);
+		float hInput = Input.GetAxis ("Horizontal"); 
+		//float vInput = Input.GetAxis ("Vertical");
 		
-				// Movement
-				// Currently movement is at a fixed speed
-				currentThrust += defaultAcceleration;
-				//transform.Translate (0, 0, currentSpeed);
-				rigidbody.AddForce (transform.forward * currentThrust);
-				
-				if (gameController.GetComponent<DebugScript> ().debug) {
-						Debug.DrawRay (transform.position, transform.forward * currentThrust);
-				}
+		// Roatation
+		float turnAmount = hInput * turnRate;
+		rigidbody.AddForceAtPosition (-transform.right * turnAmount, transform.position + transform.forward * -3);
+		//transform.Rotate (0, turnAmount, 0);
+		
+		// Movement
+		// Currently movement is at a fixed speed
+		currentThrust += defaultAcceleration;
+		//transform.Translate (0, 0, currentSpeed);
+		rigidbody.AddForce (transform.forward * currentThrust);
+		
+		if (gameController.GetComponent<DebugScript> ().debug) {
+			Debug.DrawRay (transform.position, transform.forward * currentThrust);
 		}
-
-		// When Player collides with another game object...
-		void OnTriggerEnter (Collider other)
-		{
-				// Collision with Obstacle
-				if (other.gameObject.tag.Equals ("Obstacle")) {
-						Debug.Log ("PLAYER HIT");
-						gameObject.GetComponent<PlayerController> ().InflictDamage ((int)currentThrust);
-				}
+	}
+	
+	// When Player collides with another game object...
+	void OnTriggerEnter (Collider other)
+	{		
+		// Collision with Obstacle
+		if (other.gameObject.tag.Equals ("Obstacle")) {
+			Debug.Log ("PLAYER HIT");
+			gameObject.GetComponent<PlayerController> ().InflictDamage ((int)currentThrust);
 		}
-
-		public float GetCurrentSpeed ()
-		{
-				return currentThrust;
-		}
-
-		public string GetCurrentSpeedDisplay ()
-		{
-				return currentThrust.ToString ("F1");
-		}
-
-		public void SetSinking ()
-		{
-				sinking = true;
-		}
+	}
+	
+	public float GetCurrentSpeed ()
+	{
+		return currentThrust;
+	}
+	
+	public string GetCurrentSpeedDisplay ()
+	{
+		return currentThrust.ToString ("F1");
+	}
+	
+	public void SetSinking ()
+	{
+		sinking = true;
+	}
 }
---
+++ assets/Scripts/Sound/ScrapingSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrapingSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PlaySound(){
+		audio.Play ();
+	}
+
+	void StopSound(){
+				audio.Stop ();
+		}
+
+}
